8d91eda1a132b50760ffe6debe386564ab153dd4
Egor Kutovoy <egorkincbl@gmail.com>
2024-05-23 19:31:25 +0300

    test_books.py -> test_cart.py

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -3,32 +3,32 @@ import pytest
 from libstore.store import Cart
 
 
-def test_make_empty_cart():
+def test_make_empty():
     cart = Cart()
     assert isinstance(cart.get_id(), str)
 
 
-def test_add_to_cart():
+def test_add():
     cart = Cart()
     cart.add_book('123')
     assert cart.get_books() == {'123'}
 
 
-def test_add_to_cart_multiple():
+def test_add_multiple():
     cart = Cart()
     cart.add_book('123')
     cart.add_book('333')
     assert cart.get_books() == {'123', '333'}
 
 
-def test_remove_cart_nonexistent():
+def test_remove_nonexistent():
     cart = Cart()
 
     with pytest.raises(KeyError):
         cart.remove_book('123')
 
 
-def test_remove_cart_works():
+def test_remove_works():
     cart = Cart()
 
     cart.add_book('123')
